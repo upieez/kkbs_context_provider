23afe666761c80de13be630356f6976ee0e18154
upieez <samuelhww@gmail.com>
2020-05-27 21:02:17 +0800

    boiler plate for UserState

--- src/context/user/UserState.js
+++ src/context/user/UserState.js
@@ -2,3 +2,15 @@ import React, { useReducer } from "react";
 import UserContext from "./userContext";
 import UserReducer from "./userReducer";
 import { SEARCH_USER } from "../types";
+
+const UserState = (props) => {
+	const initialState = { user: [] };
+	const [state, dispatch] = useReducer(UserReducer, initialState);
+	return (
+		<UserContext.Provider value={{ user: state.user }}>
+			{props.children}
+		</UserContext.Provider>
+	);
+};
+
+export default UserState;
