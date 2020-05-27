ff214585e16b0f1631ae33803f42026afe8c3622
upieez <samuelhww@gmail.com>
2020-05-27 20:49:28 +0800

    init commit for setup

---
+++ src/context/user/UserState.js
@@ -0,0 +1,4 @@
+import React, { useReducer } from "react";
+import UserContext from "./userContext";
+import UserReducer from "./userReducer";
+import { SEARCH_USER } from "../types";
---
+++ src/context/user/userContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+export const userContext = createContext();
+// allow me to use this file in any of the redeucer or state
